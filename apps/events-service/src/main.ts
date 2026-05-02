b727c8343e9cb2b99a5c16e54ac5238e98640e1c
PULAK-SN <pulak.official22@gmail.com>
2026-05-03 00:58:18 +0530

    adding event service

---
+++ apps/events-service/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from "@nestjs/core";
+import { EventsServiceModule } from "./events-service.module";
+import { ValidationPipe } from "@nestjs/common";
+import { SERVICES_PORT } from "@app/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create(EventsServiceModule);
+  // USE VALIDATION
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(SERVICES_PORT.EVENT_SERVICE);
+  console.log(
+    `Event service is running on port ${SERVICES_PORT.EVENT_SERVICE}`,
+  );
+}
+bootstrap();
